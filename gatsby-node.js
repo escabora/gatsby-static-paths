422aa4a98f05176eea50889fa9e580f53fdf2a3f
escabora <gabrielescabora@gmail.com>
2023-06-10 15:02:00 -0300

    refactor: function create dinamic pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const createDynamicPages = require('./index');
+
 require('@babel/register')({
   ignore: [/(node_modules)/],
   presets: ['babel-preset-gatsby'],
@@ -9,25 +11,5 @@ exports.onCreatePage = async ({ page, actions }) => {
 
   const PageComponent = require(page.component);
   const getStaticPaths = PageComponent.getStaticPaths;
-
-  if (!getStaticPaths) {
-    actions.createPage({
-      ...page,
-    });
-  } else {
-    try {
-      const routes = await getStaticPaths();
-      for (route of routes?.paths) {
-        const staticPath = route.params.basePath + route.params.slug;
-        console.log('create page ', staticPath);
-        actions.createPage({
-          ...page,
-          path: staticPath,
-          context: { staticProps: route?.staticProps },
-        });
-      }
-    } catch (err) {
-      console.error('Error gatsby-plugin-static-paths', err);
-    }
-  }
+  createDynamicPages(getStaticPaths);
 };
---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {function} getStaticPaths - Function exported from component
+ * @param {function} actions - Function of the Gatsby Node APIs
+ */
+const createDynamicPages = async (getStaticPaths, actions) => {
+  if (!getStaticPaths) {
+    actions.createPage({
+      ...page,
+    });
+  } else {
+    try {
+      const routes = await getStaticPaths();
+      for (route of routes?.paths) {
+        const staticPath = route.params.basePath + route.params.slug;
+        console.log('create page ', staticPath);
+        actions.createPage({
+          ...page,
+          path: staticPath,
+          context: { staticProps: route?.staticProps },
+        });
+      }
+    } catch (err) {
+      console.error('Error gatsby-plugin-static-paths', err);
+    }
+  }
+};
+
+export default createDynamicPages;
